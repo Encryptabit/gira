9d7cdf6fc7589b54650c727a147493335601eb8e
Jacari Jones <carithecoder@gmail.com>
2024-09-18 00:10:17 -0700

    plugin scaffholding

---
+++ scripts/content.js
@@ -0,0 +1,12 @@
+/**
+ * This is the page responsible for reading the DOM, should be injected
+ * on every page where that matches the urls in the manifest. For the early
+ * iterations of this plugin I will start with reading headers and linked issues
+ * probably. Later I may add optimizations that permit parsing an entire pag
+ * */
+
+// 1. initialize buckets, should be a dictionary with dictionaries of associations
+
+// 2. Read relevant DOM objects and parse them into a usable format
+
+// 3. Store the information in localStorage for persistance.
---
+++ scripts/popup.js
@@ -0,0 +1,30 @@
+/**
+ * This javascript is what's repsonsible for behavior in the popup.html.
+ * Should handle dropdown behavior for accordian , sorting, deleting entries
+ * etc. Going to handle everything in vanilla js for funsies
+ */
+
+// 0. Add event listener for message from background. Hydrate popup ? show loading spinner
+
+// 1. Parse data from message
+
+// 2. initialize needed dom elements with proper data.
+
+// 3. parse data from local storage.
+
+// 4. Enumerate results.
+
+
+
+/**
+ * User actions innit?
+ * autosave on user action, maintain history stack for undo? and redo
+ */
+
+// 1. Delete an entry (also handle deleting a single child from list), also Clear All
+
+// 2. Sort Alphabetically , Sort by recency bias (date added or date changed?)
+
+// (optional) split buckets by days
+
+
