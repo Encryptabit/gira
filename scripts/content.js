c44f4754bc50711d41967291b677513d2a036662
Jacari Jones <carithecoder@gmail.com>
2024-09-18 05:04:19 -0700

    created association grab

--- scripts/content.js
+++ scripts/content.js
@@ -5,8 +5,31 @@
  * probably. Later I may add optimizations that permit parsing an entire pag
  * */
 
+
+/**
+ * Needed a function that works on both versions of ticket views we encounter
+ */
+function getTicketInfoFromCloud() {
+    const parsedInfo = {};
+    const token = (window.location.href.indexOf('selectedIssue') === -1) ? '/' : '=';
+    const ticketNumber = window.location.href.split(token).pop();
+    const ticketTitle = [...document.querySelectorAll('h1[class]')].at(-1).innerText;
+
+    parsedInfo[ticketNumber] = ticketTitle;
+
+    return parsedInfo;
+}
+
+function getTicketFromPR() {
+    return 'Aw41-10579'
+}
+
 // 1. initialize buckets, should be a dictionary with dictionaries of associations
+let associationContainer = {};
+let association = {};
+let associations = [];
 
 // 2. Read relevant DOM objects and parse them into a usable format
+
 
 // 3. Store the information in localStorage for persistance.
